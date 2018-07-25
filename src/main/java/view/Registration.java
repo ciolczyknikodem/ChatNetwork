63b3ef62eb53d80923a1aa1a1e6167868a6e3446
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-07-25 15:23:21 +0200

    Add registration frame

--- src/main/java/view/Registration.java
+++ src/main/java/view/Registration.java
@@ -1,4 +1,17 @@
 package view;
 
+import javax.swing.*;
+
 public class Registration {
+    private JTextField loginTextField;
+    private JTextField passwordTextField;
+    private JButton registerButton;
+    private JButton backButton;
+    private JLabel loginField;
+    private JLabel passwordField;
+    private JPanel registrationPanel;
+
+    public JPanel getRegistrationPanel() {
+        return registrationPanel;
+    }
 }
