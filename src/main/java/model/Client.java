e23379ca038a7621edf657237b16a188ab85fa3a
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-07-25 22:36:38 +0200

    Add getters and setters for fields

--- src/main/java/model/Client.java
+++ src/main/java/model/Client.java
@@ -28,4 +28,28 @@ public class Client {
         this.login = login;
         this.password = password;
     }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
