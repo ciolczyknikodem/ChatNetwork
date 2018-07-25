f5b02815712a784d78e1bcf6e4e254be71bd0852
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-07-25 21:16:50 +0200

    Refactor, change name of components password and login

--- src/main/java/view/Registration.java
+++ src/main/java/view/Registration.java
@@ -6,12 +6,10 @@ import utils.FrameManager;
 import javax.swing.*;
 
 public class Registration {
-    private JTextField loginTextField;
-    private JPasswordField passwordTextField;
+    private JTextField loginField;
+    private JPasswordField passwordField;
     private JButton registerButton;
     private JButton backButton;
-    private JLabel loginField;
-    private JLabel passwordField;
     private JPanel registrationPanel;
 
     public Registration() {
