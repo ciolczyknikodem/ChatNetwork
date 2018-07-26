c024c015b5135493dbe139b501060ba490855d77
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-07-26 15:04:23 +0200

    Add separated run user app and server

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,13 @@
 
 import controller.MainController;
+import serverlogic.Server;
+
+import java.io.IOException;
 
 public class App {
 
     public static void main(String[] args) {
-        MainController mainController = new MainController();
+        MainController mainController = new MainController("192.168.10.208", 9000);
         mainController.runApp();
     }
 }
--- src/main/java/ServerApp.java
+++ src/main/java/ServerApp.java
@@ -4,9 +4,9 @@ import java.io.IOException;
 
 public class ServerApp {
     public static void main(String[] args) throws IOException {
-        new Server(9000, "192.168.11.70").startListening();
+        new Server(9000, "192.168.10.208").startListening();
         //Adam IPAddress = 192.168.11.70
         //Augustyn IPAddress = 192.168.10.114
-        //Nikodem IPAddress =
+        //Nikodem IPAddress = 192.168.10.208
     }
 }
