e7aeabe46cb6f63d124a3452a9c7cc47c52f3db9
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-07-26 15:40:59 +0200

    Modify Chat form/class

--- src/main/java/view/Chat.java
+++ src/main/java/view/Chat.java
@@ -1,4 +1,10 @@
 package view;
 
+import javax.swing.*;
+
 public class Chat {
+    private JButton sendButton;
+    private JPanel panel1;
+    private JButton logoutButton;
+    private JList list1;
 }
