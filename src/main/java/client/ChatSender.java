6b09f53404ef7696e4a6cc5e0ee34e178d845a77
AdamKruk91 <adam.kruk91@gmail.com>
2018-07-26 09:45:51 +0200

    Add ChatSender class

---
+++ src/main/java/client/ChatSender.java
@@ -0,0 +1,35 @@
+package client;
+
+import model.Client;
+import model.Message;
+
+import java.io.*;
+import java.net.SocketException;
+import java.util.List;
+import java.util.Scanner;
+
+public class ChatSender {
+
+    private ObjectOutputStream writer;
+    private Client client;
+    private List<Client> userList;
+
+    public ChatSender(ObjectOutputStream writer, Client client, List<Client> userList) {
+        this.client = client;
+        this.writer = writer;
+        this.userList = userList;
+    }
+
+    public void runSender() throws IOException {
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            Message message = new Message(client.getID(), scanner.nextLine());
+            try {
+                writer.writeObject(message);
+            } catch (SocketException e) {
+                System.err.println("Server is not working ...");
+                break;
+            }
+        }
+    }
+}
