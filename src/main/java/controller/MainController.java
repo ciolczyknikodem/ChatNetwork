de98079d0efd5bc46da77e0235b8c2b6a41d7f2e
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-07-26 15:07:54 +0200

    Add ipAdress and port to MainController class

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,14 +1,26 @@
 package controller;
 
+import client.ClientController;
+import model.Client;
 import utils.FrameManager;
 import view.MainFrame;
 import view.Registration;
 
 import javax.swing.*;
+import java.io.IOException;
 
 public class MainController {
     private static JFrame mainFrame;
     private static JFrame registrationFrame;
+
+    private static String ipAddress;
+    private static int port;
+    private static Client user;
+
+    public MainController(String ipAddress, int port) {
+        MainController.ipAddress = ipAddress;
+        MainController.port = port;
+    }
 
     public static void runApp() {
         mainFrame = new JFrame("SlackLike");
@@ -36,5 +48,17 @@ public class MainController {
 
     public static JFrame getRegistrationFrame() {
         return registrationFrame;
+    }
+
+    public static String getIpAddress() {
+        return ipAddress;
+    }
+
+    public static int getPort() {
+        return port;
+    }
+
+    public static void setUser(Client user) {
+        MainController.user = user;
     }
 }
