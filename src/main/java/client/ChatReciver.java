c76fcf43ab7448e1a2b556c2bde8596c68ddf219
AdamKruk91 <adam.kruk91@gmail.com>
2018-07-26 09:45:39 +0200

    Add ChatReciver class

---
+++ src/main/java/client/ChatReciver.java
@@ -0,0 +1,31 @@
+package client;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+
+public class ChatReciver {
+    private Socket socket;
+    private ObjectInputStream inputStream;
+
+    public ChatReciver(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void runListener() throws IOException {
+        inputStream = new ObjectInputStream(socket.getInputStream());
+        new Thread(() -> {
+            while (true) {
+                try {
+                    System.out.println(inputStream.readObject());
+                    System.out.println("Please type your message: ");
+                } catch (EOFException e) {
+                    break;
+                } catch (IOException | ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+}
