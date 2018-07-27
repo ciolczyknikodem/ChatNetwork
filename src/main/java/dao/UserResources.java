00fad01c9e8ab5e764801dea7f520bf7d93847e8
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-07-27 09:48:17 +0200

    Add method  get user from db

--- src/main/java/dao/UserResources.java
+++ src/main/java/dao/UserResources.java
@@ -1,6 +1,8 @@
 package dao;
 
 import model.Client;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
 
 public class UserResources extends ConnectionManager implements ManageDB {
 
@@ -19,6 +21,23 @@ public class UserResources extends ConnectionManager implements ManageDB {
             closeConnection();
         }
         return true;
+    }
+
+    public Client get(String login) {
+        Client user = null;
+
+        try {
+            initializeFactory();
+            Criteria criteria = session.createCriteria(Client.class);
+            user = (Client) criteria.add(Restrictions.eq("login", login)).uniqueResult();
+        }
+        catch (Exception e) {
+            System.out.println(e.getClass().getName() + " --> " + e.getMessage());
+        }
+        finally {
+            closeConnection();
+        }
+        return user;
     }
 
 }
