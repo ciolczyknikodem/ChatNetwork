279a4736d2115c99f4fd1a38ce42a3eafbc5382f
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-07-29 12:54:53 +0200

    Add login failed message to enum

--- src/main/java/utils/EventMessage.java
+++ src/main/java/utils/EventMessage.java
@@ -2,7 +2,10 @@ package utils;
 
 public enum EventMessage {
     REGISTER_SUCCESSFUL("Registration successful!"),
-    REGISTER_FAILED("Registration failed!");
+    REGISTER_FAILED("Registration failed!"),
+    LOGIN_SUCCESSFUL("Login successful!"),
+    LOGIN_FAILED("Login failed!")
+    ;
 
     private String message;
     EventMessage(String message) {
