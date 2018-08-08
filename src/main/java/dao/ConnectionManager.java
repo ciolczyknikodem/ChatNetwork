f584afab8d716a3c77f31880ae581335e1c181eb
AdamKruk91 <adam.kruk91@gmail.com>
2018-08-08 14:13:04 +0200

    Change initialising factory to do it once in lifecycle

--- src/main/java/dao/ConnectionManager.java
+++ src/main/java/dao/ConnectionManager.java
@@ -4,30 +4,22 @@ import model.User;
 import model.Message;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class ConnectionManager {
 
     protected SessionFactory sessionFactory;
-    protected Transaction transaction;
     protected Session session;
 
+    public ConnectionManager() {
+        initializeFactory();
+    }
 
     protected void initializeFactory() {
         sessionFactory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(User.class)
                 .addAnnotatedClass(Message.class)
-                .buildSessionFactory();
-
-        session = sessionFactory.getCurrentSession();
-        transaction = session.beginTransaction();
-
-    }
-
-    protected void closeConnection() {
-        session.close();
-        sessionFactory.close();
+                .buildSessionFactory(); // todo find a way to close connection
     }
 }
