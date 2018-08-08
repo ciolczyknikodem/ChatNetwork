ef25a449d3fcfb967d1fe25f7cca59932beffee4
AdamKruk91 <adam.kruk91@gmail.com>
2018-08-08 12:23:37 +0200

    Change IP Address to localhost for tests

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,10 @@
 
 import controller.MainController;
-import serverlogic.Server;
-
-import java.io.IOException;
 
 public class App {
 
     public static void main(String[] args) {
-        MainController mainController = new MainController("192.168.10.208", 9000);
+        MainController mainController = new MainController("localhost", 9000);
         mainController.runApp();
     }
 }
--- src/main/java/ServerApp.java
+++ src/main/java/ServerApp.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class ServerApp {
     public static void main(String[] args) throws IOException {
-        new Server(9000, "192.168.10.208").startListening();
+        new Server(9000, "localhost").startListening();
         //Adam IPAddress = 192.168.11.70
         //Augustyn IPAddress = 192.168.10.114
         //Nikodem IPAddress = 192.168.10.208
